a52b8cc7f0b4c07995ab3c0965404b59aead5eb9
venkatesh2257 <vinodjc007@gmail.com>
2026-06-16 20:43:35 +0530

    this is hello world program

---
+++ src/Main.kt
@@ -0,0 +1,9 @@
+var name:String ? = "this is the name testing "
+
+fun main() {
+    println("Hello World!")
+    println(name)
+    name = "this is the quality test "
+    println(name)
+    // here i am writing the hello world and after this i am going to create the project in the main development
+}
