4b1f37d43b352fec10b9b9547a0b2e74a9026a6e
venkatesh2257 <vinodjc007@gmail.com>
2026-06-16 21:01:15 +0530

    this is functions creating

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
+this is day one i am learning kotlin and i executed hello programming by hands not using the AI.
 this venkatesh i am learning this kotlin .
--- src/Main.kt
+++ src/Main.kt
@@ -1,9 +1,7 @@
-var name:String ? = "this is the name testing "
-
-fun main() {
-    println("Hello World!")
-    println(name)
-    name = "this is the quality test "
-    println(name)
-    // here i am writing the hello world and after this i am going to create the project in the main development
+fun getGreeting(): String {
+    return "Hello, Kotlin!"
+}
+fun main(){
+    println("Hello, world")
+    println(getGreeting())
 }
